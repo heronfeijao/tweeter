79ad33c6c13a38595a1829df2c08ca9855682986
Heron Feijao <heronfeijao@gmail.com>
2022-04-26 18:19:30 -0300

    tweet count added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  $('.tweetform').on('input', function(event) {
+    const text = $(this).val().length;
+    const counter = $(this).parent().find('.counter');
+    counter.val(() => {
+      const count = 140;
+      if (count - text < 0) {
+        $(counter).css("color", "red");
+      }
+      if (count - text >= 0) {
+        $(counter).css("color", "");
+      }
+      return count - text;
+    });
+  });
+
+});
+
